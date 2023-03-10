f1a8a0ba0a8aba4b82dc07c1103285b6d7cfaf86
Sofiia Shmaiun <sofiia.shmaiun@gmail.com>
2023-03-11 01:03:24 +0200

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+var _ = require('lodash');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+}
+
+player.on('timeupdate', _.throttle(onPlay, 1000));
+
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
